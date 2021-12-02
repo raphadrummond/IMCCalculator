be988d22381d676087f2ca6e2458060409dfde6c
raphadrummond <raphavenanciodrummond@gmail.com>
2021-12-01 23:23:49 -0300

    Projeto de APP de Calculadora de IMC

---
+++ app/src/main/java/com/dnommurd/imccalculator/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.dnommurd.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
